083c38f9f40f5c162ca934afb9a22dce664e4bc1
Oğuz Şahin <Osahnn@outlook.com>
2024-02-27 15:56:48 +0300

    update GameContext

--- Assets/_Project/Scripts/App/GameContext.cs
+++ Assets/_Project/Scripts/App/GameContext.cs
@@ -1,4 +1,5 @@
-﻿using Inputs;
+﻿using Data;
+using Inputs;
 using Player;
 using Rogue_Enemy;
 using Service_Locator;
@@ -9,17 +10,19 @@ namespace App
 {
     public class GameContext : MonoBehaviour
     {
+        [SerializeField] private GameDataManager _saveLoadManager;
         [SerializeField] private PlayerController _playerController;
         [SerializeField] private InputSystem _inputSystem;
         [SerializeField] private EnemyManager _enemyManager;
 
         public void Init()
         {
-            _playerController.Init(_inputSystem);
+            _playerController.Init(_inputSystem, _saveLoadManager);
             _enemyManager.Init(_playerController);
 
             ServiceProvider.Instance.
-                Register<IEnemyManager>(_enemyManager);
+                Register<IEnemyManager>(_enemyManager).
+                Register<IInputSystem>(_inputSystem);
         }
     }
 
