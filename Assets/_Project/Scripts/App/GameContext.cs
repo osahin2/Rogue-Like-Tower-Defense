da2db07e639945ca52f2ab16d660aef2dbe92a38
Oğuz Şahin <Osahnn@outlook.com>
2024-03-14 18:20:34 +0300

    add game flow update context update app

--- Assets/_Project/Scripts/App/GameContext.cs
+++ Assets/_Project/Scripts/App/GameContext.cs
@@ -3,26 +3,38 @@ using Inputs;
 using Player;
 using Rogue_Enemy;
 using Service_Locator;
-using System;
+using UIScripts;
 using UnityEngine;
 
 namespace App
 {
     public class GameContext : MonoBehaviour
     {
-        [SerializeField] private GameDataManager _saveLoadManager;
+        [SerializeField] private GameDataManager _gameDataManager;
         [SerializeField] private PlayerController _playerController;
         [SerializeField] private InputSystem _inputSystem;
         [SerializeField] private EnemyManager _enemyManager;
-
-        public void Init()
+        [SerializeField] private UIManager _uiManager;
+        public IGameDataManager GameDataManager => _gameDataManager;
+        public UIManager UIManager => _uiManager;
+        public PlayerController PlayerController => _playerController;
+        public EnemyManager EnemyManager => _enemyManager;
+        public void Construct()
         {
-            _playerController.Init(_inputSystem, _saveLoadManager);
-            _enemyManager.Init(_playerController);
+            RegisterInstances();
 
+            _gameDataManager.Construct();
+            _playerController.Construct(_inputSystem, _gameDataManager);
+            _enemyManager.Construct(_playerController);
+            _uiManager.Construct();
+
+        }
+
+        private void RegisterInstances()
+        {
             ServiceProvider.Instance.
-                Register<IEnemyManager>(_enemyManager).
-                Register<IInputSystem>(_inputSystem);
+                Register<IInputSystem>(_inputSystem).
+                Register<IGameDataManager>(_gameDataManager);
         }
     }
 
--- Assets/_Project/Scripts/App/GameManager.cs
+++ Assets/_Project/Scripts/App/GameManager.cs
@@ -1,18 +1,76 @@
 using EventBusSystem;
-using Service_Locator;
-using System.Collections;
-using System.Collections.Generic;
+using MenuEvent;
+using Rogue.GameEvent;
+using UIScripts;
 using UnityEngine;
 
 namespace App
 {
+    public enum GameState
+    {
+        Play,
+        MainMenu,
+        Pause
+    }
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private GameContext _gameContext;
 
+        private UIManager _uiManager;
+
         private void Awake()
         {
-            _gameContext.Init();
+            Application.targetFrameRate = 60;
+
+            _gameContext.Construct();
+            _uiManager = _gameContext.UIManager;
+        }
+        private void Start()
+        {
+            _uiManager.OpenMenu();
+        }
+
+        private void OnBattleStart(MenuEvents.BattleButton args)
+        {
+            _uiManager.CloseMenu();
+            _gameContext.EnemyManager.Init();
+            _gameContext.PlayerController.Init();
+        }
+
+        private void OnBattleEnd(GameplayEvents.GameEnd args)
+        {
+            _uiManager.OpenMenu();
+            _gameContext.EnemyManager.DeInit();
+            _gameContext.PlayerController.DeInit();
+        }
+
+        private void OnEnable()
+        {
+            AddEvents();
+        }
+        private void OnDisable()
+        {
+            RemoveEvents();
+        }
+
+        private void OnApplicationPause(bool pause)
+        {
+            _gameContext.GameDataManager.Save();
+        }
+        private void OnApplicationQuit()
+        {
+            _gameContext.GameDataManager.Save();
+        }
+
+        private void AddEvents()
+        {
+            EventBus<MenuEvents.BattleButton>.RegisterEvent(MenuEvents.BATTLE_BUTTON_CLICK, OnBattleStart);
+            EventBus<GameplayEvents.GameEnd>.RegisterEvent(GameplayEvents.GAME_END, OnBattleEnd);
+        }
+        private void RemoveEvents()
+        {
+            EventBus<MenuEvents.BattleButton>.DeRegisterEvent(MenuEvents.BATTLE_BUTTON_CLICK, OnBattleStart);
+            EventBus<GameplayEvents.GameEnd>.DeRegisterEvent(GameplayEvents.GAME_END, OnBattleEnd);
         }
     }
 }
