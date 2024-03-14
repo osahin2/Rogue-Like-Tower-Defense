a36729d2299325475de55d7eda6962d8483c915f
Oğuz Şahin <Osahnn@outlook.com>
2024-03-14 16:45:28 +0300

    add general animator controller

---
+++ Assets/_Project/Scripts/AnimatorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField] private float _crossFadeDuration = .1f;
+
+    private Animator _animator;
+
+    public void Init()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    public float PlayAnimation(int animationHash) 
+    {
+        if (!_animator.enabled)
+        {
+            return 0f;
+        }
+        _animator.CrossFade(animationHash, _crossFadeDuration);
+        return _animator.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    public void SetActiveAnimator(bool active) => _animator.enabled = active;
+}
