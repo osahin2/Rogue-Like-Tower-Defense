2ae19027bfac7a0a97b7065bfe881378136980ea
Oğuz Şahin <Osahnn@outlook.com>
2024-03-14 16:41:51 +0300

    add Health system

---
+++ Assets/_Project/Scripts/CharacterHealth/Health.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HealthSystem
+{
+    public class Health : IHealth
+    {
+        private float _maxHealth;
+        public float CurrentHealth { get; private set; }
+        public float HealthPercentage => CurrentHealth / _maxHealth;
+        public Health(float maxHealth)
+        {
+            _maxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+        }
+
+        public void Decrease(float amount, Action onReducedZero)
+        {
+            CurrentHealth -= amount;
+            if (CurrentHealth <= 0)
+            {
+                onReducedZero?.Invoke();
+                CurrentHealth = 0;
+            }
+        }
+        public void Increase(float amount)
+        {
+            CurrentHealth += amount;
+
+            if (CurrentHealth >= _maxHealth)
+            {
+                CurrentHealth = _maxHealth;
+            }
+        }
+
+        public void Reset()
+        {
+            CurrentHealth = _maxHealth;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/CharacterHealth/IHealth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HealthSystem
+{
+    public interface IHealth
+    {
+        public float HealthPercentage { get; }
+        void Decrease(float amount, Action onReducedZero);
+        void Increase(float amount);
+        void Reset();
+    }
+}
