72082d02dc9bcd03d4369c931023cc6959d19c3d
Oğuz Şahin <Osahnn@outlook.com>
2024-03-14 18:18:47 +0300

    add non alloc raycast class

---
+++ Assets/_Project/Scripts/NonAllocRaycaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Player.Weapons
+{
+    public class NonAllocRaycaster
+    {
+        public bool Raycast(Vector2 origin, Vector2 direction, RaycastHit2D[] results, float distance, int layer)
+        {
+            var hits = Physics2D.RaycastNonAlloc(
+                origin,
+                direction,
+                results,
+                distance,
+                layer);
+            if (hits == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
